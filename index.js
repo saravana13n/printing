8667f240f87b9f46e3c2c3ad35112eefc3baa218
saravana13n <saravana13n@gmail.com>
2018-03-16 15:59:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,16 @@ var app = {
             type: "share",         //Open a context menu and ask the user what to do next (print, mail, etc..).
             fileName: 'v8-tutorial.pdf' //it will use this filename as a place-holder
         }
-        pdf.fromURL('http://cesarvr.github.io/2015/11/20/javascript-v8.html', opts)
-        .then((status) => console.log('success->', status))
-        .catch((error) => console.log(error));
+        pdf.htmlToPDF({
+                        data: "<!DOCTYPE html><html><title>HTML Tutorial</title><body><h1>This is a heading</h1><p>This is a paragraph.</p></body></html>",
+                        documentSize: 'A4',
+                        landscape: 'portrait',
+                        type: 'share' //use share to open the open-with-menu.
+                    }, this.success, this.failure)
+        
+//         pdf.fromURL('http://cesarvr.github.io/2015/11/20/javascript-v8.html', opts)
+//         .then((status) => console.log('success->', status))
+//         .catch((error) => console.log(error));
   // It end here
         console.log('Received Event: ' + id);
     }
